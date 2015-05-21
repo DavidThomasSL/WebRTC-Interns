508fe4744814ea68b26c9941e233b59991c11e65
dthomas <dthomas@scottlogic.co.uk>
2015-05-21 15:05:56 +0000

    Change to be a SimpleWebRTC demo

---
+++ client/js/client.js
@@ -0,0 +1,19 @@
+var webrtc = new SimpleWebRTC({
+    
+  peerConnectionConfig: {
+    iceServers: [{"url": "stun.callwithus.com:3478"}]
+  },
+    
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'localVideo',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remotesVideos',
+  // immediately ask for camera access
+  autoRequestMedia: true
+});
+
+// we have to wait until it's ready
+webrtc.on('readyToCall', function () {
+  // you can name it anything
+  webrtc.joinRoom('GOTEAMDAVE');
+});
